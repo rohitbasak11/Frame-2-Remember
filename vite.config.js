dfb24bfe5dd801910930afbc61cf128b955b31b6
Rohit Basak <111831963+rohitbasak11@users.noreply.github.com>
2026-04-11 01:02:09 +1200

    fix: added vite.config.js to build multi-page app for Vercel

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        enquire: resolve(__dirname, 'enquire.html'),
+        declaration: resolve(__dirname, 'declaration.html'),
+      },
+    },
+  },
+});
